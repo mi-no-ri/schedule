d7f980409687b4cc8f6907ed91969b3450cd92ad
実 <harada.miyu.0901@gmail.com>
2026-06-25 00:17:21 +0900

    Plansコントローラー作成、ルーティング追加

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,30 @@
+class PlansController < ApplicationController
+  # 予定一覧画面
+  def index
+  end
+
+  # 予定新規追加画面
+  def new
+  end
+
+  # 予定DB登録処理
+  def create
+  end
+
+  # 予定詳細画面
+  def show
+  end
+
+  # 予定編集画面
+  def edit
+  end
+
+  # 予定更新処理
+  def update
+  end
+
+  # 予定削除処理
+  def destroy
+  end
+
+end
