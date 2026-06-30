e20a8c7df16bbe492f92769dd11179a92ab913e5
実 <harada.miyu.0901@gmail.com>
2026-07-01 04:53:35 +0900

    予定の新規追加画面実装(CSS除く)

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -6,10 +6,18 @@ class PlansController < ApplicationController
 
   # 予定新規追加画面
   def new
+    @plan = Plan.new
   end
 
   # 予定DB登録処理
   def create
+    @plan = Plan.new(plan_params)
+
+    if @plan.save
+      redirect_to @plan, notice: "予定を追加しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # 予定詳細画面
@@ -28,4 +36,11 @@ class PlansController < ApplicationController
   def destroy
   end
 
+  private
+
+  # 許可するキーだけを受け取る
+  def plan_params
+    params.require(:plan).permit(:name, :start, :end, :all_day)
+  end
+  
 end
--- db/migrate/20260624161846_create_plans.rb
+++ db/migrate/20260624161846_create_plans.rb
@@ -1,7 +1,7 @@
 class CreatePlans < ActiveRecord::Migration[7.2]
   def change
     create_table :plans do |t|
-      t.string :title
+      t.string :name
       t.datetime :start
       t.datetime :end
       t.boolean :all_day
