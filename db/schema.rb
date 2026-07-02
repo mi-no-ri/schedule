19def8f309923c47695f562643c12e0ffbdf3f42
実 <harada.miyu.0901@gmail.com>
2026-07-03 01:31:07 +0900

    不要なマイグレーションファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_07_01_132145) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_30_205615) do
   create_table "plans", force: :cascade do |t|
     t.string "name"
     t.datetime "start_day"
